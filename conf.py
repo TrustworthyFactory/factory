024b17df5eb1eaa19c54d07689eba5bc4848d9ad
cdanger <cdanger@users.noreply.github.com>
2016-01-14 18:57:49 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -11,3 +11,28 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 project = 'Trustworthy Factory'
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The encoding of source files.
+#source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = 'contents'
+
+# General information about the project.
+project = u'AuthZForce'
+copyright = u'2015, Thales Communications and Security'
+author = u'Frederic Motte'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '4.4.1'
+# The full version, including alpha/beta/rc tags.
+release = '4.4.1-FIWARE-R4'
